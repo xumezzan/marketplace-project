fefd13da9631b7d9dc348232211444a653a8f27a
Khumoyun Izzatov <whomaun@gmail.com>
2025-11-17 19:09:43 +0500

    Fix accounts app, custom user and db confg

--- backend/marketplace/migrations/0001_initial.py
+++ backend/marketplace/migrations/0001_initial.py
@@ -11,6 +11,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
+        ('accounts', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
